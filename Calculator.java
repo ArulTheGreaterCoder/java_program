bf778a9d85a33a2b2cd97a8de787819b35cd2136
ArulTheGreaterCoder <arulkansal04@gmail.com>
2025-12-18 20:36:35 +0530

    8

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class Calculator{
     public static void add(int num1, int num2){
-        System.out.println("Addition:",num1+num2);
+        System.out.println("Addition:"+(num1+num2));
     }
     public static void subtract(int num1, int num2){
-        System.out.println("Subtraction:",num1-num2);
+        System.out.println("Subtraction:"+ (num1-num2));
     }
-    public static void multiple(int num1, int num2){
-        System.out.println("Multiplication:",num1*num2);
+    public static void multiply(int num1, int num2){
+        System.out.println("Multiplication:"+(num1*num2));
     }
     public static void divide(int num1, int num2){
-        System.out.println("Division:",num1/num2);
+        System.out.println("Division:"+(num1/num2));
     }
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
@@ -21,22 +21,21 @@ public class Calculator{
         System.out.println("Enter your second number:");
         int num2 = scanner.nextInt();
         if (choice == 1){
-            int add;
+            add(num1,num2);
         } else if (choice == 2){
-            int subtract;
+            subtract(num1,num2);
         } else if (choice == 3){
-            int multiple;
+            multiply(num1,num2);
         } else if (choice == 4){
-            int divide;
+            divide(num1,num2);
         }else{
             System.out.println("Enter your correct choice:");
         }
         String input = scanner.nextLine();
-        if (input == "y" || input == "Y"){
-            choice = scanner.nextInt();
+        if (input.equals("Y") || input.equals("y")){
+            System.out.println("Restart the program.");
         }else{
             System.out.println("Bye.");
         }
     }
-    
 }
